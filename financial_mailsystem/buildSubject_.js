5592ad5f404b3bc98399c8cb4d44ffaf6deec7d9
deaningithub <t104318516@ntut.org.tw>
2026-05-14 11:49:56 +0800

    Update default report subject

--- financial_mailsystem/buildSubject_.js
+++ financial_mailsystem/buildSubject_.js
@@ -1,5 +1,5 @@
 function buildSubject_(report, subscriber) {
-  const base = report["信件標題"] || "每日盤中財經時事報告";
+  const base = report["信件標題"] || "每日盤中財經時事快報";
 
   if (subscriber.isExpiringSoon) {
     return "【剩 " + subscriber.daysLeft + " 天到期】" + base;
